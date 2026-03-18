3fc864a75a40d92f1bd8921a63e87cde86e6e59e
Amaiadigital25 <tuemail@gmail.comamaiadigital25>
2026-03-17 22:55:08 -0300

    Primer commit - estructura base backend y frontend

---
+++ frontend/app.js
@@ -0,0 +1,36 @@
+let token = "";
+
+async function login() {
+  const user = document.getElementById("user").value;
+  const pass = document.getElementById("pass").value;
+
+  const res = await fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ user, pass })
+  });
+
+  const data = await res.json();
+
+  if (data.token) {
+    token = data.token;
+
+    document.getElementById("login").style.display = "none";
+    document.getElementById("dashboard").style.display = "block";
+  } else {
+    alert("Error de login");
+  }
+}
+
+async function getData() {
+  const res = await fetch("http://localhost:3000/data", {
+    headers: {
+      "Authorization": token
+    }
+  });
+
+  const data = await res.json();
+  document.getElementById("data").innerText = JSON.stringify(data, null, 2);
+}
