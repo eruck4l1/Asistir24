c0cc2e5c174152eb3bce4bd98d9d3c24bad8b30a
Amaiadigital25 <tuemail@gmail.comamaiadigital25>
2026-03-18 05:20:17 -0300

    fix: mover frontend a root para deploy

---
+++ app.js
@@ -0,0 +1,30 @@
+function login() {
+  const user = document.getElementById("user").value;
+  const role = document.getElementById("role").value;
+
+  if (!user) {
+    alert("Ingresá un usuario");
+    return;
+  }
+
+  localStorage.setItem("user", user);
+  localStorage.setItem("role", role);
+
+  if (role === "admin") {
+    window.location.href = "admin.html";
+  } else {
+    window.location.href = "dashboard.html";
+  }
+}
+
+// AUTO LOGIN
+(function () {
+  const path = window.location.pathname;
+
+  if (path.includes("dashboard") || path.includes("admin")) {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      window.location.href = "index.html";
+    }
+  }
+})();
