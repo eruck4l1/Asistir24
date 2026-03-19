35c613ffd7f5a51263d6ec53f5b3e7d3b45afe3f
Amaiadigital25 <tuemail@gmail.comamaiadigital25>
2026-03-19 17:28:58 -0300

    version final limpia

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 function login() {
   const user = document.getElementById("user").value;
-  const pass = document.getElementById("pass").value;
   const role = document.getElementById("role").value;
 
-  if (!user || !pass) {
-    alert("Completar todos los campos");
+  if (!user) {
+    alert("Ingresá usuario");
     return;
   }
 
-  alert(`Bienvenido ${user} (${role})`);
-
-  // FUTURO: conectar con backend
+  if (role === "admin") {
+    window.location.href = "admin.html";
+  } else if (role === "prestador") {
+    window.location.href = "prestador.html";
+  } else {
+    window.location.href = "cliente.html";
+  }
 }
